abcffd58f59eef8953c43e5cdc6d1911417183ad
Adam Pietrzykowski <adam.a.pietrzykowski@gmail.com>
2017-12-30 16:35:48 +0100

    changed from parsing html to parsing api and added cron info to readme file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ada-logger
-A Cardano (ADA) value logger written in Ruby. Work in progress.
+A Cardano (ADA) value logger written in Ruby. It gets the current Cardano price from the CoinMarketCap API and logs it to the file. 
+It also displays the percental change in price since the last hour.
+
+## How to use
+The best way to use the logger is to execute the crontab command. First, edit the existing crontab or create a new one:
+```
+crontab -e
+```
+
+Then, simply add a new task. I would like the script to be executed every hour, so I'm going to write:
+```
+0 * * * * <path to my script>
+```
--- ada-logger.rb
+++ ada-logger.rb
@@ -1,20 +1,23 @@
-require 'rubygems'
-require 'mechanize'
-require 'open-uri'
+require 'net/http'
+require 'json'
 
-def get_value
-  mechanize = Mechanize.new
-  page = mechanize.get("https://coinmarketcap.com/currencies/cardano/")
-  return page.at('div.container div.row div.col-lg-10 div.row.bottom-margin-1x div.col-xs-6.col-sm-8.col-md-4.text-left span#quote_price span.text-large2').text.strip
+def parse
+  url = 'https://api.coinmarketcap.com/v1/ticker/cardano/?convert=EUR'
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  parsed = JSON.parse(response)
+  
+  return parsed
 end
 
 def log_message(value)
   t = Time.now.asctime
-  return "Cardano (ADA) - $" + value + " - " + t + "\n"
+  return "Cardano (ADA) cost $" + parse.first['price_usd'] + " as of " + t + 
+    ". The price has changed " + parse.first['percent_change_1h'] + "% since last hour."
 end
 
 def write_to_file(message)
   File.open("ada-log.txt", 'a+') {|f| f.puts(message)}
 end
 
-write_to_file(log_message(get_value))
+write_to_file(log_message(parse))

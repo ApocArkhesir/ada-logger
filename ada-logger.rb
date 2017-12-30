7dcac086975fc13f9f0f5e70f7b221750121e945
Adam Pietrzykowski <adam.a.pietrzykowski@gmail.com>
2017-12-30 01:43:29 +0100

    initial commit

---
+++ ada-logger.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'mechanize'
+require 'open-uri'
+
+def get_value
+  mechanize = Mechanize.new
+  page = mechanize.get("https://coinmarketcap.com/currencies/cardano/")
+  return page.at('div.container div.row div.col-lg-10 div.row.bottom-margin-1x div.col-xs-6.col-sm-8.col-md-4.text-left span#quote_price span.text-large2').text.strip
+end
+
+def log_message(value)
+  t = Time.now.asctime
+  return "Cardano (ADA) - $" + value + " - " + t + "\n"
+end
+
+def write_to_file(message)
+  File.open("ada-log.txt", 'a+') {|f| f.puts(message)}
+end
+
+while true
+  write_to_file(log_message(get_value))
+  sleep 60
+end
